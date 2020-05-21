8c66dc8747b86da28865973f3366ead1f9b3df7f
André Viana <andrebermudesviana@gmail.com>
2020-05-21 15:40:09 -0300

    acho que tá certo

--- graficos.py
+++ graficos.py
@@ -4,66 +4,169 @@ import matplotlib.animation as animation
 from matplotlib.animation import FuncAnimation
 from matplotlib.pylab import *
 
-def plotAnimations(i, v, LEN, TIME):
+def plotAnimations(i, v, LEN, TIME, dz, tomarMedia, velocidade, intervalo):
     plt.style.use('seaborn-pastel')
 
-    # Seta algumas configurações
-    #font = {'size' : 9}
-    #matplotlib.rc('font', **font)
+    # Cria as figuras
+    estatVolt = figure(num = 1, figsize = (8, 6))
+    estatCurr = figure(num = 2, figsize = (8, 6))
+    anim = figure(num = 0, figsize = (8, 6))
 
-    # Configura a janela (figure) e os gráficos (subplots)
-    f0 = figure(num = 0, figsize = (8, 6))
-    #f0.suptitle("Tensao e Corrente", fontsize=10)
-    volt = subplot2grid((2, 1), (0, 0))
-    curr = subplot2grid((2, 1), (1, 0))
-    #tight_layout()
+    # Nomeia as figuras
+    anim.canvas.set_window_title('Animações')
+    estatVolt.canvas.set_window_title('Gráficos Estáticos da Tensao')
+    estatCurr.canvas.set_window_title('Gráficos Estáticos da Corrente')
 
-    # Configura os titulos
-    volt.set_title('Tensao')
-    curr.set_title('Corrente')
+    #Cria os subplots de cada figura
+    voltAnim = subplot2grid((2, 1), (0, 0), fig=anim)
+    currAnim = subplot2grid((2, 1), (1, 0), fig=anim)
+
+    voltMiddle = subplot2grid((3, 1), (0, 0), fig=estatVolt)
+    voltEnd = subplot2grid((3, 1), (1, 0), fig=estatVolt)
+    voltEstationary = subplot2grid((3, 1), (2, 0), fig=estatVolt)
+    
+    currMiddle = subplot2grid((3, 1), (0, 0), fig=estatCurr)
+    currEnd = subplot2grid((3, 1), (1, 0), fig=estatCurr)
+    currEstationary = subplot2grid((3, 1), (2, 0), fig=estatCurr)
+
+    # Configura os titulos dos subplots
+    if(tomarMedia):
+        voltAnim.set_title('Tensão (com filtro de média)')
+        currAnim.set_title('Corrente (com filtro de média)')
+
+        voltMiddle.set_title('Tensão após Percorrer Metade da Linha de Transmissão (com filtro de média)')
+        voltEnd.set_title('Tensão após uma Reflexão (com filtro de média)')
+        voltEstationary.set_title('Tensão no Regime Estacionário (com filtro de média)')
+
+        currMiddle.set_title('Corrente após Percorrer Metade da Linha de Transmissão (com filtro de média)')
+        currEnd.set_title('Corrente após uma Reflexão (com filtro de média)')
+        currEstationary.set_title('Corrente no Regime Estacionário (com filtro de média)')
+    else:
+        voltAnim.set_title('Tensão')
+        currAnim.set_title('Corrente')
+
+        voltMiddle.set_title('Tensão após Percorrer Metade da Linha de Transmissão')
+        voltEnd.set_title('Tensão após uma Reflexão')
+        voltEstationary.set_title('Tensão no Regime Estacionário')
+
+        currMiddle.set_title('Corrente após Percorrer Metade da Linha de Transmissão')
+        currEnd.set_title('Corrente após uma Reflexão')
+        currEstationary.set_title('Corrente no Regime Estacionário')
 
     # Seta os limites para o eixo y
-    volt.set_ylim(0,3)
-    curr.set_ylim(0,0.05)
+    voltAnim.set_ylim(-3,3)
+    currAnim.set_ylim(-0.05,0.05)
+
+    voltMiddle.set_ylim(-3,3)
+    voltEnd.set_ylim(-3,3)
+    voltEstationary.set_ylim(-3,3)
+
+    currMiddle.set_ylim(-0.05,0.05)
+    currEnd.set_ylim(-0.05,0.05)
+    currEstationary.set_ylim(-0.05,0.05)
 
     # Seta os limites para o eixo x
-    volt.set_xlim(0,LEN)
-    curr.set_xlim(0,LEN)
+    voltAnim.set_xlim(0,LEN)
+    currAnim.set_xlim(0,LEN+1)
+
+    voltMiddle.set_xlim(0,LEN)
+    voltEnd.set_xlim(0,LEN)
+    voltEstationary.set_xlim(0,LEN)
+
+    currMiddle.set_xlim(0,LEN+1)
+    currEnd.set_xlim(0,LEN+1)
+    currEstationary.set_xlim(0,LEN+1)
+
+    # Seta os ticklabels para o eixo x
+    voltAnim.set_xticklabels(voltAnim.get_xticks()/LEN)
+    currAnim.set_xticklabels(currAnim.get_xticks()/LEN)
+
+    voltMiddle.set_xticklabels(voltMiddle.get_xticks()/LEN)
+    voltEnd.set_xticklabels(voltEnd.get_xticks()/LEN)
+    voltEstationary.set_xticklabels(voltEstationary.get_xticks()/LEN)
+
+    currMiddle.set_xticklabels(currMiddle.get_xticks()/LEN)
+    currEnd.set_xticklabels(currEnd.get_xticks()/LEN)
+    currEstationary.set_xticklabels(currEstationary.get_xticks()/LEN)
 
     # Aciona o modo 'grid'
-    volt.grid(True)
-    curr.grid(True)
+    voltAnim.grid(True)
+    currAnim.grid(True)
+    
+    voltMiddle.grid(True)
+    voltEnd.grid(True)
+    voltEstationary.grid(True)
+
+    currMiddle.grid(True)
+    currEnd.grid(True)
+    currEstationary.grid(True)
 
     # Nomeia os eixos
-    volt.set_xlabel("m")
-    volt.set_ylabel("tensao")
-    curr.set_xlabel("m")
-    curr.set_ylabel("corrente")
+    voltAnim.set_xlabel("Posição (m)")
+    voltAnim.set_ylabel("Tensão (V)")
+    currAnim.set_xlabel("Posição (m)")
+    currAnim.set_ylabel("Corrente (A)")
+
+    voltMiddle.set_xlabel("Posição (m)")
+    voltMiddle.set_ylabel("Tensão (V)")
+    voltEnd.set_xlabel("Posição (m)")
+    voltEnd.set_ylabel("Tensão (V)")
+    voltEstationary.set_xlabel("Posição (m)")
+    voltEstationary.set_ylabel("Tensão (V)")
+
+    currMiddle.set_xlabel("Posição (m)")
+    currMiddle.set_ylabel("Corrente (A)")
+    currEnd.set_xlabel("Posição (m)")
+    currEnd.set_ylabel("Corrente (A)")
+    currEstationary.set_xlabel("Posição (m)")
+    currEstationary.set_ylabel("Corrente (A)")
 
     # Vetores utilizados como eixo x
     tensaoEixoX= np.arange(LEN)
     correnteEixoX= np.arange(LEN+1)
 
-    # Inicializa os gráficos
-    p011, = volt.plot(tensaoEixoX, v[0], 'r-', label="tensao")
-    p021, = curr.plot(correnteEixoX, i[0], 'b-', label="corrente")
+    # Ajeita o Layout
+    anim.tight_layout()
+    estatVolt.tight_layout()
+    estatCurr.tight_layout()
 
-    # Configura as legendas
-    volt.legend([p011], [p011.get_label()])
-    curr.legend([p021], [p021.get_label()])
+    # Inicializa os gráficos
+    if(tomarMedia):
+        p011, = voltAnim.plot(np.convolve(v[0], np.ones(5)*(1/5),mode="same"), 'r-')
+        p021, = currAnim.plot(np.convolve(i[0], np.ones(5)*(1/5),mode="same"), 'b-')
+
+        p111, = voltMiddle.plot(np.convolve(v[TIME//20], np.ones(5)*(1/5),mode="same"), 'r-')
+        p121, = voltEnd.plot(np.convolve(v[TIME//10 + TIME//40], np.ones(5)*(1/5),mode="same"), 'r-')
+        p131, = voltEstationary.plot(np.convolve(v[TIME - 1], np.ones(5)*(1/5),mode="same"), 'r-')
+
+        p211, = currMiddle.plot(np.convolve(i[TIME//20], np.ones(5)*(1/5),mode="same"), 'b-')
+        p221, = currEnd.plot(np.convolve(i[TIME//10 + TIME//40], np.ones(5)*(1/5),mode="same"), 'b-')
+        p231, = currEstationary.plot(np.convolve(i[TIME - 1], np.ones(5)*(1/5),mode="same"), 'b-')
+    else:
+        p011, = voltAnim.plot(v[0], 'r-')
+        p021, = currAnim.plot(i[0], 'b-')
+
+        p111, = voltMiddle.plot(v[TIME//20], 'r-')
+        p121, = voltEnd.plot(v[TIME//10 + TIME//40], 'r-')
+        p131, = voltEstationary.plot(v[TIME - 1], 'r-')
+
+        p211, = currMiddle.plot(i[TIME//20], 'b-')
+        p221, = currEnd.plot(i[TIME//10 + TIME//40], 'b-')
+        p231, = currEstationary.plot(i[TIME - 1], 'b-')
 
     # Função que atualiza a animação
     def updateData(n):
-
-        p011.set_data(tensaoEixoX, v[n])
-        p021.set_data(correnteEixoX, i[n])
+        if(tomarMedia):
+            p011.set_data(tensaoEixoX, np.convolve(v[n*velocidade], np.ones(5)*(1/5),mode="same"))
+            p021.set_data(correnteEixoX, np.convolve(i[n*velocidade], np.ones(5)*(1/5),mode="same"))
+        else:
+            p011.set_data(tensaoEixoX, v[n*velocidade])
+            p021.set_data(correnteEixoX, i[n*velocidade])
 
         return p011, p021
 
-    # interval: desenhe um novo frame a cada 'interval' ms
-    # frames: numero de frames a serem desenhados
-    simulation = animation.FuncAnimation(f0, updateData, blit=False, frames=TIME, interval=1, repeat=False)
+    simulation = animation.FuncAnimation(anim, updateData, blit=True, frames=TIME//velocidade, interval=intervalo, repeat=False)
 
     plt.show()
-    # Uncomment the next line if you want to save the animation
-    #simulation.save(filename='sim.mp4',fps=30,dpi=300)
+    # Descomente a próxima linha se deseja salvar a animação
+    # simulation.save(filename='animacao.mp4',fps=30,dpi=300)
