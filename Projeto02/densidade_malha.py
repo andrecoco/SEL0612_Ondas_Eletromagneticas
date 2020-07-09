be7dbfd559a8be6c0e86636da2a7d94d6f380af7
André Viana <andrebermudesviana@gmail.com>
2020-07-09 15:24:11 -0300

    umas alterações pro plot do segundo gráfico

--- Projeto02/densidade_malha.py
+++ Projeto02/densidade_malha.py
@@ -16,7 +16,7 @@ PI = math.pi
 NUM_PONTOS = 1000
 S = 0.5
 MIN_N = 1
-MAX_N = 10
+MAX_N = 80
 
 def velocidade_fase(S, N):
     """
@@ -47,24 +47,34 @@ for i, n in enumerate(Ns):
 
 erros = abs(1-velocidades)*100
 
-##### Plot do gráfico #####
 
-#Configura a figura
+##### Plot dos gráficos #####
+#Configura as figuras
 fig, plotAtenuacao = plt.subplots()
-fig.canvas.set_window_title('Nome da Figura')
-fig.suptitle('Titulo da Figura', fontsize=16)
+fig.canvas.set_window_title('Figura 1')
+fig.suptitle('Variação da Velocidade de Fase da Onda e a Atenuação', fontsize=14)
 plotVelocidade = plotAtenuacao.twinx()
 
-#Plota os dois graficos
-plotVelocidade.plot(Ns, velocidades, 'g-')
-plotAtenuacao.plot(Ns, atenuacoes, '--')
+fig2, plotErro = plt.subplots()
+fig2.canvas.set_window_title('Figura 2')
+fig2.suptitle('Erro da Velocidade de Fase', fontsize=14)
+
+#Plota os dados
+plotVelocidade.plot(Ns, velocidades, 'g-', color = 'red')
+plotAtenuacao.plot(Ns, atenuacoes, '--', color = 'blue')
+#Para o plot do Erro vamos utilzar um subarray para N entre 3-80
+plotErro.plot(Ns[int((3-MIN_N)*NUM_PONTOS/(MAX_N-MIN_N)) :], erros[int((3-MIN_N)*NUM_PONTOS/(MAX_N-MIN_N)) :], color = 'green')
 
 # Seta os limites para o eixo x
 plotAtenuacao.set_xlim(1,10)
+plotErro.set_xlim(0,80)
 
 # Seta os limites para os eixos y
 plotVelocidade.set_ylim(0, 2)
 plotAtenuacao.set_ylim(0, 6)
+plotErro.set_ylim(0.01, 100)
+#Seta a escala do eixo y para log no grafico do erro
+plotErro.set_yscale('log')
 
 #Seta os ticks
 plotAtenuacao.xaxis.set_tick_params(which="major", top = True, direction = "in")
@@ -82,13 +92,23 @@ plotVelocidade.yaxis.set_tick_params(which="minor", right = True, direction = "i
 plotVelocidade.yaxis.set_major_locator(ticker.AutoLocator())
 plotVelocidade.yaxis.set_minor_locator(ticker.AutoMinorLocator())
 
-#Coloca um texto acima de cada gráfico
+plotErro.xaxis.set_tick_params(which="major", top = True, direction = "in")
+plotErro.xaxis.set_tick_params(which="minor", top = True, direction = "in")
+plotErro.xaxis.set_major_locator(ticker.AutoLocator())
+plotErro.xaxis.set_minor_locator(ticker.AutoMinorLocator())
+
+plotErro.yaxis.set_tick_params(which="major", right = True, direction = "in")
+plotErro.yaxis.set_tick_params(which="minor", right = True, direction = "in")
+
+#Coloca um texto acima dos plots
 plotVelocidade.text(4, 1.05, 'Velocidade de Fase da Onda Numérica')
 plotAtenuacao.text(3, 1.10, 'Constante de Atenuação')
 
 #Nomeia os eixos
-plotAtenuacao.set_xlabel('Densidade da Grade (points per free-space wavelength)')
+plotAtenuacao.set_xlabel('Densidade da Grade (pontos por comprimento de onda)')
+plotAtenuacao.set_ylabel('Constante de Atenuação (neppers/celula da grade)')
 plotVelocidade.set_ylabel('Velocidade de Fase da Onda Numérica (normalizada em c)')
-plotAtenuacao.set_ylabel('Constante de Atenuação (neppers/grid cell)')
+plotErro.set_xlabel('Densidade da Grade (pontos por comprimento de onda)')
+plotErro.set_ylabel('Erro da Velocidade de Fase (%)')
 
 plt.show()
