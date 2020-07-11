0c59e113703789a5ad04a9935d4ce8854d0ab57a
André Viana <andrebermudesviana@gmail.com>
2020-07-11 15:57:40 -0300

    Add files via upload

--- Projeto03/caso_2D.py
+++ Projeto03/caso_2D.py
@@ -44,7 +44,7 @@ AnimZmin = -0.8           # Valor máximo do eixo Z da animação
 
 #precisão do comprimento
 dx = 1e-2  # m
-dt = dx/(np.sqrt(2)*c)  # s
+dt = 1*dx/(np.sqrt(2)*c)  # s
 
 #lado do quadrado do espaço (Quantidade de pontos simulados)
 LEN = int(l/dx) #pontos
@@ -58,11 +58,11 @@ assert (memoria < 2*(2**30)), "parâmetros consomem muita memoria: " + str(memor
 
 # Ez no centro da grade
 # Pulso retangular
-#Ez_t = np.zeros(TIME)
-#Ez_t[0:int(0.1*(l/c)/dt)] = 1
+Ez_t = np.zeros(TIME)
+Ez_t[0:int(0.1*(l/c)/dt)] = 1
 # Onda senoidal
-Ez_t = np.linspace(0, 2*np.pi, num=TIME)
-Ez_t = np.sin(2*Ez_t*(TIME/(0.5*(l/c)/dt)))
+#Ez_t = np.linspace(0, 2*np.pi, num=TIME)
+#Ez_t = np.sin(2*Ez_t*(TIME/(0.5*(l/c)/dt)))
 
 # Condições iniciais
 Ez0 = np.zeros((LEN+1, LEN+1))
@@ -110,49 +110,86 @@ animacao2D.plotAnimations(Ez, LEN, TIME, AnimZmin, AnimZmax)
 
 #Cria as Figuras Estaticas
 fig1, ax1 = plt.subplots()
+fig1_2, ax1_2 = plt.subplots()
 fig2, ax2 = plt.subplots()
+fig2_2, ax2_2 = plt.subplots()
 fig3, ax3 = plt.subplots()
-fig4, ax4 = plt.subplots()
-fig1.canvas.set_window_title('Ez')
-fig1.suptitle('Componente z do Campo E', fontsize=12)
-fig2.canvas.set_window_title('Hx')
-fig2.suptitle('Componente x do Campo H', fontsize=12)
-fig3.canvas.set_window_title('Hy')
-fig3.suptitle('Componente y do Campo H', fontsize=12)
-fig4.canvas.set_window_title('Ez 3D')
-fig4.suptitle('Componente z do Campo E (visualização em 3D)', fontsize=12)
+fig3_2, ax3_2 = plt.subplots()
+fig4 = plt.figure()
+ax4 = fig4.gca(projection='3d')
+fig4_2 = plt.figure()
+ax4_2 = fig4_2.gca(projection='3d')
 
-#Plota FIG1
-maxval = np.max(abs(Ez[-1]))
-colormap = ax1.imshow(Ez[-1], cmap=COR, vmin=-maxval, vmax=maxval)
+fig1.canvas.set_window_title('Ez_antes')
+fig1.suptitle('Componente z do Campo E (antes do final da grid)', fontsize=12)
+fig1_2.canvas.set_window_title('Ez_depois')
+fig1_2.suptitle('Componente z do Campo E (após chegar no final da grid)', fontsize=12)
+fig2.canvas.set_window_title('Hx_antes')
+fig2.suptitle('Componente x do Campo H (antes do final da grid)', fontsize=12)
+fig2_2.canvas.set_window_title('Hx_depois')
+fig2_2.suptitle('Componente x do Campo H (após chegar no final da grid)', fontsize=12)
+fig3.canvas.set_window_title('Hy_antes')
+fig3.suptitle('Componente y do Campo H (antes do final da grid)', fontsize=12)
+fig3_2.canvas.set_window_title('Hy_depois')
+fig3_2.suptitle('Componente y do Campo H (após chegar no final da grid)', fontsize=12)
+fig4.canvas.set_window_title('Ez 3D_antes')
+fig4.suptitle('Componente z do Campo E (visualização em 3D antes do final da grid)', fontsize=12)
+fig4_2.canvas.set_window_title('Ez 3D_depois')
+fig4_2.suptitle('Componente z do Campo E (visualização em 3D após chegar no final da grid)', fontsize=10)
+
+#Plota Ez 
+maxval = np.max(abs(Ez[int(0.6*TIME)]))
+colormap = ax1.imshow(Ez[int(0.6*TIME)], cmap=COR, vmin=-maxval, vmax=maxval)
 ax1.set_xlabel('x')
 ax1.set_ylabel('y')
 fig1.colorbar(colormap)
+maxval = np.max(abs(Ez[-1]))
+colormap = ax1_2.imshow(Ez[-1], cmap=COR, vmin=-maxval, vmax=maxval)
+ax1_2.set_xlabel('x')
+ax1_2.set_ylabel('y')
+fig1_2.colorbar(colormap)
 
-#Plota FIG2
-maxval = np.max(abs(Hx[-1]))
-colormap = ax2.imshow(Hx[-1], cmap=COR, vmin=-maxval, vmax=maxval)
+#Plota Hx
+maxval = np.max(abs(Hx[int(0.6*TIME)]))
+colormap = ax2.imshow(Hx[int(0.6*TIME)], cmap=COR, vmin=-maxval, vmax=maxval)
 ax2.set_xlabel('x')
 ax2.set_ylabel('y')
 fig2.colorbar(colormap)
+maxval = np.max(abs(Hx[-1]))
+colormap = ax2_2.imshow(Hx[-1], cmap=COR, vmin=-maxval, vmax=maxval)
+ax2_2.set_xlabel('x')
+ax2_2.set_ylabel('y')
+fig2_2.colorbar(colormap)
 
-#Plota FIG3
-maxval = np.max(abs(Hy[-1]))
-colormap = ax3.imshow(Hy[-1], cmap=COR, vmin=-maxval, vmax=maxval)
+#Plota Hy
+maxval = np.max(abs(Hy[int(0.6*TIME)]))
+colormap = ax3.imshow(Hy[int(0.6*TIME)], cmap=COR, vmin=-maxval, vmax=maxval)
 ax3.set_xlabel('x')
 ax3.set_ylabel('y')
 fig3.colorbar(colormap)
+maxval = np.max(abs(Hy[-1]))
+colormap = ax3_2.imshow(Hy[-1], cmap=COR, vmin=-maxval, vmax=maxval)
+ax3_2.set_xlabel('x')
+ax3_2.set_ylabel('y')
+fig3_2.colorbar(colormap)
 
-#Plota FIG4
+#Plota Ez 3D
 x = np.linspace(-100, 100, 101)
 y = np.linspace(-100, 100, 101)
 X, Y = np.meshgrid(x, y)
-Z = Ez[-1]
-ax4 = plt.axes(projection='3d')
-ax4.plot_surface(X, Y, Z, rcount = 200 , ccount = 200,  cmap=COR)
+ax4.plot_surface(X, Y, Ez[int(0.6*TIME)], rcount = 200 , ccount = 200,  cmap=COR)
 ax4.set_xlabel('x')
 ax4.set_ylabel('y')
 ax4.set_zlabel('V/m')
+ax4_2.plot_surface(X, Y, Ez[-1], rcount = 200 , ccount = 200,  cmap=COR)
+ax4_2.set_xlabel('x')
+ax4_2.set_ylabel('y')
+ax4_2.set_zlabel('V/m')
+#Calcula e define os limites do eixo Z
+zmax = np.maximum(np.amax(Ez[int(0.6*TIME)]), np.amax(Ez[-1]))
+zmin = np.minimum(np.amin(Ez[int(0.6*TIME)]), np.amin(Ez[-1]))
+ax4.set_zlim(zmin, zmax)
+ax4_2.set_zlim(zmin, zmax)
 
 plt.show()
 
